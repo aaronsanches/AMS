52c9a0e71fe61e25b048ee808b8cf156d4114c74
Glen D'souza <gdsouza@linuxmail.org>
2018-02-13 16:19:56 +0530

    Add templates and static storage
    * Add static storage to root
    * Add project-wide template DIR

--- AMS/AMS/settings.py
+++ AMS/AMS/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'AMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # User Model
 
