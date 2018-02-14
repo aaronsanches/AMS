11e8de2e77b65b1bfcde9fa01e8740c0069cf3fb
Aaron Sanches <aaronsanches@gmail.com>
2018-02-14 23:32:35 +0530

    Initial site-wide navbar. Added css for navbar. Updated accounts urls. Updated base template.

--- AMS/accounts/urls.py
+++ AMS/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
 
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'accounts'
 
 urlpatterns = [
+  path('login/', auth_views.login, name='login'),
+  path('logout/', auth_views.logout, name='logout'),
   path('', include('django.contrib.auth.urls')),
   path('index.html', views.index, name='index'),
 ]
