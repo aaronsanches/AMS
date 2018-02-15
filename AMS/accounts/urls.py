0052e8b9674211400c360985c7597feeb35752a9
AS <aaronsanches@gmail.com>
2018-02-15 12:59:05 +0530

    NavBar and Register View
    * Initial site-wide navbar
    Added CSS for navbar.
    Updated accounts urls.
    Updated base template.
    
    * Added register view.
    Updated urls.

--- AMS/accounts/urls.py
+++ AMS/accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import include, path
 
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'accounts'
 
 urlpatterns = [
+  path('login/', auth_views.login, name='login'),
+  path('logout/', auth_views.logout, name='logout'),
+  path('register/', views.register, name='register'),
   path('', include('django.contrib.auth.urls')),
-  path('index.html', views.index, name='index'),
+  path('index/', views.index, name='index'),
 ]
--- AMS/accounts/views.py
+++ AMS/accounts/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import PersonCreationForm
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, World. You're at the accounts index.")
 
+def register(request):
+    if request.method == 'POST':
+        form = PersonCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('accounts:index')
+    else:
+        form = PersonCreationForm()
+    return render(request, 'registration/register.html', {'form':form})
