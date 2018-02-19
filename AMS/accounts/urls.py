eacfce494d602f84578fbdd1d67a6b97bdceb1cf
Aaron Sanches <aaronsanches@gmail.com>
2018-02-19 23:05:11 +0530

    Added password reset through email. Removed 'password change'. Updated urls and redirects.

--- AMS/accounts/urls.py
+++ AMS/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from . import views
@@ -5,7 +6,12 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-  path('', include('django.contrib.auth.urls')),
-  path('register/', views.register, name='register'),
-  path('index/', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('password_reset/', auth_views.password_reset, {'post_reset_redirect':'accounts:password_reset_done', }, name='password_reset'),
+    #path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.password_reset_confirm, {'post_reset_redirect':'accounts:password_reset_complete', }, name='password_reset_confirm'),
+    #path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
+    path('profile/', views.profile, name="profile"),
 ]
