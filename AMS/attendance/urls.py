a21568cb278bb8e671229d4500aa3c356686e537
Glen D'souza <gdsouza@linuxmail.org>
2018-04-30 09:48:10 +0530

    Correctly namespace urls
    * Put attendance under own subdirectory
    * Change naming of views
    * Start with DetailView for individual object

--- AMS/attendance/urls.py
+++ AMS/attendance/urls.py
@@ -6,7 +6,7 @@ app_name = 'attendance'
 
 urlpatterns = [
     path('courses/', CourseList.as_view(), name='course-list'),
-    path('attendance/', AttendanceList.as_view(), name='attendance-list'),
-    # path('attendanceDetails/', AttendanceDetails.as_view(), name='detail'),
+    path('list/', AttendanceList.as_view(), name='attendance-list'),
+    path('details/<pk>/', AttendanceDetails.as_view(), name='detail'),
     path('add/', AttendanceWizard.as_view([AttendanceForm1, AttendanceForm2]), name='create'),
 ]
